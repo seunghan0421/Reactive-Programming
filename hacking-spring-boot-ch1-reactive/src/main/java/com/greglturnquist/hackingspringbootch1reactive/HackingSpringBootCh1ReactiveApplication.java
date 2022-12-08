b1ddad6f15d17a3548c90a6acd853b86898ac593
SeungHan <jk05018@naver.com>
2022-12-08 16:09:00 +0900

    Chapter1: Webflux project start

---
+++ hacking-spring-boot-ch1-reactive/src/main/java/com/greglturnquist/hackingspringbootch1reactive/HackingSpringBootCh1ReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.hackingspringbootch1reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackingSpringBootCh1ReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackingSpringBootCh1ReactiveApplication.class, args);
+	}
+
+}
