269fcad6ef47630e9905c02881dbac8e92be94fd
SeungHan <jk05018@naver.com>
2022-12-10 16:03:49 +0900

    chapter2: Blocking 방식으로 동작하는 코드 제거
    BlockingItemRepository, RepositoryDatabaseLoader 제거
    mongodb-sync dependency 추가

--- ecommerce/src/main/java/com/greglturnquist/ecommerce/component/TemplateDatabaseLoader.java
+++ ecommerce/src/main/java/com/greglturnquist/ecommerce/component/TemplateDatabaseLoader.java
@@ -2,19 +2,19 @@ package com.greglturnquist.ecommerce.component;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Component;
 
 import com.greglturnquist.ecommerce.domain.Item;
-import com.greglturnquist.ecommerce.repository.BlockingItemRepository;
 
 @Component
-public class RepositoryDatabaseLoader {
+public class TemplateDatabaseLoader {
 
 	@Bean
-	CommandLineRunner initialize(BlockingItemRepository repository){
+	CommandLineRunner initialize(MongoOperations mongo){
 		return args -> {
-			repository.save(new Item("Alf alarm clock", 19.99));
-			repository.save(new Item("Smurf TV tray", 24.99));
+			mongo.save(new Item("Alf alarm clock", 19.99));
+			mongo.save(new Item("Smurf TV tray", 24.99));
 		};
 	}
 }
